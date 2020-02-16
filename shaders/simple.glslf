b3c28562a846cc1dc27db0201f56fe48b5ddb34a
Joshua Foster <josh2112@gmail.com>
2020-02-16 11:37:58 -0500

    we have achieved fruit stand

--- shaders/simple.glslf
+++ shaders/simple.glslf
@@ -6,5 +6,5 @@ in vec2 v_Uv;
 out vec4 Target0;
 
 void main() {
-    Target0 = texture( t_Texture, v_Uv );
+    Target0 = texture( t_Texture, vec2( v_Uv.x, 1-v_Uv.y ));
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@
 use gfx::traits::FactoryExt;
 use gfx::Device;
 
+use std::path::Path;
+
+#[macro_use]
+extern crate arrayref;
+
 use cgmath::{Matrix4,Vector3,Point3,Deg};
 use cgmath::prelude::One;
+use cgmath::InnerSpace;
+use image;
+use tobj;
 
 type ColorFormat = gfx::format::Rgba8;
 type DepthFormat = gfx::format::DepthStencil;
@@ -26,10 +34,9 @@ gfx_defines! {
         constants: gfx::ConstantBuffer<Constants> = "Constants",
         tex: gfx::TextureSampler<[f32; 4]> = "t_Texture",
         out: gfx::RenderTarget<ColorFormat> = "Target0",
+        out_depth: gfx::DepthTarget<DepthFormat> = gfx::preset::depth::LESS_EQUAL_WRITE,
     }
 }
-
-use image;
 
 fn load_texture<F, R>(factory: &mut F, path: &str) -> Result<gfx::handle::ShaderResourceView<R, [f32; 4]>, String>
     where F: gfx::Factory<R>, R: gfx::Resources
@@ -56,7 +63,7 @@ pub fn main() {
     let context = glutin::ContextBuilder::new().with_vsync( true );
     let mut event_loop = glutin::EventsLoop::new();
     
-    let (window_ctx, mut device, mut factory, mut color_view, mut depth_view) =
+    let (window_ctx, mut device, mut factory, color_view, depth_view) =
         gfx_window_glutin::init::<ColorFormat, DepthFormat>( builder, context, &event_loop )
         .expect( "Failed to create window!" );
 
@@ -68,36 +75,40 @@ pub fn main() {
         pipe::new()
     ).expect( "Failed to create pipeline state object!" );
 
-    const SQ_VERTICES: &[Vertex] = &[
-        Vertex { pos: [0.5, -0.5, 0.0], uv: [1.0, 1.0] },
-        Vertex { pos: [0.5, 0.5, 0.0], uv: [1.0, 0.0] },
-        Vertex { pos: [-0.5, 0.5, 0.0], uv: [0.0, 0.0] },
-        Vertex { pos: [-0.5, -0.5, 0.0], uv: [0.0, 1.0] },
-    ];
-    const SQ_INDICES: &[u16] = &[0, 1, 2, 2, 3, 0];
+    let (models, _) = tobj::load_obj( &Path::new( "resources/obj/stall.obj"))
+        .expect( "Failed to load model!" );
+    let stall_model = models.iter().next().unwrap();
 
+    let stall_vertices = (0..stall_model.mesh.positions.len()/3)
+        .map( |n| Vertex {
+            pos: array_ref![stall_model.mesh.positions, n*3, 3].clone(),
+            uv: array_ref![stall_model.mesh.texcoords, n*2, 2].clone()
+        } ).collect::<Vec<Vertex>>().into_boxed_slice();
+        
     let mut locals: Constants = Constants {
         model: Matrix4::one().into(),
         view: Matrix4::look_at(
-            Point3::new(0.0, 0.0, 2.0),
+            Point3::new(10.0, 10.0, -10.0),
             Point3::new(0.0, 0.0, 0.0),
             Vector3::unit_y(),
         ).into(),
         proj: Matrix4::one().into(),
     };
+    
 
-    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice( SQ_VERTICES, SQ_INDICES );
+    let (vertex_buffer, slice) = factory.create_vertex_buffer_with_slice( &stall_vertices, &stall_model.mesh.indices as &[u32] );
     let locals_buffer = factory.create_constant_buffer( 1 );
-    let texture = load_texture( &mut factory, "resources/img/donutface.jpg" ).expect( "Failed to load texture image!" );
+    let texture = load_texture( &mut factory, "resources/img/stall.png" ).expect( "Failed to load texture image!" );
     
     let mut data = pipe::Data {
         vbuf: vertex_buffer,
         tex: (texture, factory.create_sampler_linear()),
         constants: locals_buffer,
         out: color_view,
+        out_depth: depth_view,
     };
 
-    let mut angle: f64 = 0.0;
+    let mut angle: f32 = 0.0;
 
     let mut running = true;
     while running {
@@ -112,23 +123,21 @@ pub fn main() {
                     } | CloseRequested => running = false,
                     Resized( size ) => {
                         window_ctx.resize( size.to_physical( window_ctx.get_hidpi_factor()));
-                        gfx_window_glutin::update_views( &window_ctx, &mut data.out, &mut depth_view );
+                        gfx_window_glutin::update_views( &window_ctx, &mut data.out, &mut data.out_depth );
                         let aspect = (size.width/size.height) as f32;
-                        locals.proj = cgmath::perspective( Deg(60.0f32), aspect, 0.1, 1000.0).into();
+                        locals.proj = cgmath::perspective( Deg(60.0f32), aspect, 0.1, 1000.0 ).into();
                     },
                     _ => (),
                 }
             }
         });
 
-        angle += 0.001;
-        let (angle_cos, angle_sin) = (angle.cos() as f32, angle.sin() as f32);
-        locals.model[0][0] = angle_cos;
-        locals.model[1][1] = angle_cos;
-        locals.model[0][1] = angle_sin;
-        locals.model[1][0] = -angle_sin;
+        angle += 0.5;
+        //locals.model = Matrix4::from_axis_angle( Vector3::new( 1f32, 1f32, 1f32 ).normalize(), Deg(angle) ).into();
+        //locals.model = Matrix4::from_angle_z( Deg( angle )).into();
 
         encoder.clear( &data.out, [0.2, 0.0, 0.0, 1.0] );
+        encoder.clear_depth( &data.out_depth, 1.0 );
         encoder.update_buffer( &data.constants, &[locals], 0 ).expect( "Failed to update transform!" );
         encoder.draw( &slice, &pso, &data );
         
